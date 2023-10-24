fc50a310e95c22aa29e4aca08d6fdcaceee0aa0e
vinhnam191 <vinhnam191@gmail.com>
2023-10-24 22:26:24 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PhotosComponent } from './photos/photos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

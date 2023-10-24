1ff0e6dd6222c6796dfbf01be78dd8f55d1a3a77
vinhnam191 <vinhnam191@gmail.com>
2023-10-24 22:47:01 +0700

    create modules and component

---
+++ src/app/modules/item-detail/item-detail.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemDetailComponent } from './item-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    ItemDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ItemDetailModule { }
---
+++ src/app/modules/item-list/item-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemListComponent } from './item-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ItemListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ItemListModule { }

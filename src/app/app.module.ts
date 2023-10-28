3b6b4692a65061119cd94736deb5c6870deec051
vinhnam191 <vinhnam191@gmail.com>
2023-10-28 14:05:39 +0700

    remove: footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ItemModule } from './modules/item/item.module';
 import { ItemListModule } from './modules/item-list/item-list.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,7 +11,7 @@ import { CommonModule } from '@angular/common';
 import { ItemDetailModule } from './modules/item-detail/item-detail.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     CommonModule,
